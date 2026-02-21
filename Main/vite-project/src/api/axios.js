2529bbe5c0d71eb20b5b76a5b52c856346ce03fe
PINAK V <pinakdv11@gmail.com>
2026-02-21 15:11:13 +0530

    commit

--- Main/vite-project/src/api/axios.js
+++ Main/vite-project/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://electrical-shop-10.onrender.com",
   withCredentials: true,
 });
 
